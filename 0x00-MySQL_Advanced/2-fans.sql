e15a6cf712c51a855dffc3b6e8ea1532437e79ad
Mdlalose4life <blessing1059@gmail.com>
2023-07-15 20:26:45 +0200

    2-fans.sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- script that ranks country origins of bands.
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: origin and nb_fans
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
