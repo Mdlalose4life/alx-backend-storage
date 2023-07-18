7a4b48245b80fe15be1bec07d878f76a63b599e7
Mdlalose4life <blessing1059@gmail.com>
2023-07-18 23:47:11 +0200

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -11,4 +11,4 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """ Insets a user value to kwarg """
-    return mongo_collection.inset(kwargs)
+    return mongo_collection.insert(kwargs)
