b1db2e87d1ec03ae292e285e893c7cd2454730e7
Mdlalose4life <blessing1059@gmail.com>
2023-07-18 23:48:50 +0200

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+"""
+A Python function that changes all topics of a school document based on the name:
+
+Prototype: def update_topics(mongo_collection, name, topics):
+mongo_collection will be the pymongo collection object
+name (string) will be the school name to update
+topics (list of strings) will be the list of topics approached in the school
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """updates all topics of a school document"""
+    return mongo_collection.update_many({"name": name},
+                                        {"$set": {"topics": topics}})
