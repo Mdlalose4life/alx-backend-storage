fbf888d1d9a9af8782ca6f7be67917f05cf0ce27
Mdlalose4life <blessing1059@gmail.com>
2023-07-20 01:47:55 +0200

    The files

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import redis
+import uuid
+from typing import Any, Union, Optional, Callable
+class Cache:
+    """Cache class.
+    """
+
+    def __init__(self):
+        """
+        Cache class constructor that initializes a Redis client instance
+        and flushes the database with flushdb.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Method to store data in Redis and return a key for the stored
+        data.
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to store. Can
+            be a str, bytes, int, or float.
+
+        Returns:
+            str: A randomly generated key that can be used to retrieve
+            the stored data.
+        """
+        # generate a random key using uuid
+        keys = str(uuid.uuid4())
+        # store the data in Redis using the key
+        self._redis.set(keys, data)
+        # return the key
+        return keys
